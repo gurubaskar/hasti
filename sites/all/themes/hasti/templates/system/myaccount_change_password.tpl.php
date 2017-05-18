240279f10e2cbbb414c0ff0fc523db92ad975dad
Baskaran B <baskaran.b@sonata-software.com>
2017-05-18 16:29:17 +0530

    Change password template

--- sites/all/themes/hasti/templates/system/myaccount_change_password.tpl.php
+++ sites/all/themes/hasti/templates/system/myaccount_change_password.tpl.php
@@ -1,4 +1,4 @@
-<div class="">
+<!--<div class="">
   <div class="">
     <div class="">
       <label for="OLD_PASSWORD"><span class="myaccount-label"><span class="required">*</span>
@@ -21,5 +21,19 @@
   </div>
   <div class="">
     <input type="button" name="" id="js_submitChangePasswdBtn" value="Save">
+  </div>
+</div>-->
+
+<div id="content">
+  <div class="container-fluid">
+    <div data-role="" id="changepwd">
+      <h3>Change Password</h3>
+      <input type="password" maxlength="30" class="password" name="OLD_PASSWORD" id="OLD_PASSWORD" value="" placeholder="* Current Password">
+      <input type="password" maxlength="30" class="password" name="NEW_PASSWORD" id="PASSWORD" value="" placeholder="* New Password">
+      <input type="password" maxlength="30" class="password" name="CONFIRM_PASSWORD" id="CONFIRM_PASSWORD" value="" placeholder="* Confirm Password">
+      <div class="forgot-btn">
+        <input type="button" value="Save" id="js_submitChangePasswdBtn">
+      </div>
+    </div>
   </div>
 </div>
