16e09e80ec4e43e5b152602358bb660254196902
Baskaran B <baskaran.b@sonata-software.com>
2017-05-19 18:20:23 +0530

    remove item from wishlist

--- sites/all/modules/drubiz/templates/myaccount_love_list.tpl.php
+++ sites/all/modules/drubiz/templates/myaccount_love_list.tpl.php
@@ -1,5 +1,6 @@
 
-<?php //krumo($wishlist)?>
+<!-- <?php //krumo($wishlist)?>100074-1 -->
+<?php echo "as ". get_user_party_id().'--'.get_user_jsessionid();?>
 <div id="eCommerceShowWishList" class="eCommerceShowWishList">
   <?php if(empty($wishlist['wishListItemDetails'])){?>
     <div class="no-items-wishlist">
@@ -34,19 +35,30 @@
                <?php echo $product->product_name;?>
               </h4>
             </a>
-            <div class="cartrow"><label>Qty:</label><span class="qty"><?php echo $wishList_value['quantity'];?></span></div>
+            <div class="cartrow"><label>Qty:</label><span class="qty"><?php echo round($wishList_value['quantity']);?></span></div>
             <div class="cartrow"><label>Price:</label><span>Rs. <?php echo format_money($product->list_price); ?></span></div>
             <div class="cartrow"><label>Size:</label><span class="size"><?php echo @$selected_features['Size'] ?></span></div>
             <div class="cartrow"><label>Color:</label><span class="color"><?php echo @$selected_features['Color'] ?></span></div>
-            <div class="cartrow"><span>In Stock</span></div>
+            <?php $inventoryCheck = pdp_out_of_stock_info($product_variant->product_id);?>
+            <div class="cartrow">
+              <?php if($inventoryCheck->inventory == 'Yes') {?>
+              <span>In Stock</span>
+              <?php } else { ?>
+              <span>Out Stock</span>
+              <?php } ?>
+            </div>
           </div>
-          <div class="details-right">
-            <a href="#" class="remove">Remove</a>
+          <div class="wishList_cls_btn wish-delete" data-delete-id="delete_<?php echo $wishList_value['shoppingListItemSeqId'];?>">
+            <a class="remove" title="Remove Item" href="#">Remove</a>
           </div>
           <div class="btns-wrap">
             <span>
+              <?php if($inventoryCheck->inventory == 'Yes') {?>
               <a class="standardBtn addToCart addToCartFromWishlistGlobular" data-delete-id="delete_<?php echo $wishList_value['shoppingListItemSeqId'];?>" data-product-id= "<?php echo $product_variant->product_id; ?>" data-quantity="<?php echo round($wishList_value['quantity']);?>" title="Add to Cart">
               Add to Bag</a>
+              <?php } else { ?>
+              <a class="" title="Add to Cart" disabled="disabled"> Add to Bag </a>
+              <?php } ?>
               </span>
           </div>
         </div>
