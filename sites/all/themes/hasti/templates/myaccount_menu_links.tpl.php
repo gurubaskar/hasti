443f305195f4efa209d2eea616a3e95b71d718cc
Baskaran B <baskaran.b@sonata-software.com>
2017-07-17 18:09:41 +0530

    active link

--- sites/all/themes/hasti/templates/myaccount_menu_links.tpl.php
+++ sites/all/themes/hasti/templates/myaccount_menu_links.tpl.php
@@ -1,27 +1,52 @@
+<?php
+
+$arg = arg(1);
+$active = "active";
+$myOrder = "";
+$trackOrder = "";
+$review = "";
+$myWallet = "";
+$personalInfo = "";
+$changePwd = "";
+$addressClass = "";
+
+if($arg == 'orders' || $arg == 'returnOrder' || $arg == 'reorder') {
+	$myOrder = $active;
+}
+if($arg == 'profile') {
+	$personalInfo = $active;
+}
+if($arg == 'add-address' || $arg == 'address-book' || $arg == 'edit-address') {
+	$addressClass = $active;
+}
+if($arg == 'change-password') {
+	$changePwd = $active;
+}
+?>
 <h2>My Account</h2>
 <ul>
 	<li>
 		<h3>Orders</h3>
 		<ul>
-			<li><a href="<?php echo url('account/orders');?>" data-ajax="false">My Order</a></li>
-			<li><a href="<?php echo url('account/');?>" data-ajax="false">Track Order</a></li>
+			<li><a href="<?php echo url('account/orders');?>" data-ajax="false" class="<?php echo $myOrder;?>" >My Order</a></li>
+			<li><a href="<?php echo url('account/');?>" data-ajax="false" class="<?php echo $trackOrder;?>">Track Order</a></li>
 		</ul>
 	</li>
 	<li>
 		<h3>My Stuff</h3>
 		<ul>
-			<li><a href="<?php echo url('account/');?>" data-ajax="false">My Review & Ratings</a></li>
+			<li><a href="<?php echo url('account/');?>" data-ajax="false" class="<?php echo $review;?>">My Review & Ratings</a></li>
 			<!--<li><a href="<?php //echo url('account/');?>" data-ajax="false">Recommendations for you</a></li>-->
 			<li><a href="<?php echo url('account/love-list');?>" data-ajax="false">My Wishlist</a></li>
-			<li><a href="<?php echo url('account/');?>" data-ajax="false">My Wallet</a></li>
+			<li><a href="<?php echo url('account/');?>" data-ajax="false" class="<?php echo $myWallet;?>">My Wallet</a></li>
 		</ul>
 	</li>
 	<li>
 		<h3>Settings</h3>
 		<ul>
-			<li><a href="<?php echo url('account/profile');?>" data-ajax="false">Personal information</a></li>
-			<li><a href="<?php echo url('account/change-password');?>" data-ajax="false">Change Password</a></li>
-			<li><a href="<?php echo url('account/address-book');?>" data-ajax="false">Address</a></li>
+			<li><a href="<?php echo url('account/profile');?>" data-ajax="false" class="<?php echo $personalInfo;?>">Personal information</a></li>
+			<li><a href="<?php echo url('account/change-password');?>" data-ajax="false" class="<?php echo $changePwd;?>">Change Password</a></li>
+			<li><a href="<?php echo url('account/address-book');?>" data-ajax="false" class="<?php echo $addressClass;?>">Address</a></li>
 			<!--<li><a href="<?php //echo url('account/');?>" data-ajax="false">Profile Settings</a></li>
 			<li><a href="<?php //echo url('account/');?>" data-ajax="false">Update Email/Mobile</a></li>
 			<li><a href="<?php //echo url('account/');?>" data-ajax="false">Deactivate Account</a></li>
