d77b51380540e922e672630f88308ebcc194ad83
Sanjana T G <sanjana.tg@SONATA.LOCAL>
2018-01-08 12:16:37 +0530

    pdd tabs

--- sites/all/themes/hasti/templates/pdp_raw_artisans.tpl.php
+++ sites/all/themes/hasti/templates/pdp_raw_artisans.tpl.php
@@ -1,5 +1,8 @@
 <?php 
 $ofbiz_url = variable_get("drubiz_ofbiz_url");
+if(count($getRawMaterials['artisans']) == 0){?>
+<p> No data FOund.</p>
+<?php }else{
 foreach ($getRawMaterials['artisans'] as $keyRawMaterials => $valueRawMaterials) :?>
 
 <div id="content" class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
@@ -33,34 +36,8 @@ foreach ($getRawMaterials['artisans'] as $keyRawMaterials => $valueRawMaterials)
             </div>
           </div>
         </div> <!-- content-wrap end -->
-
-        <div class="content-wrap"> <!-- content-wrap -->
-          <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 left">
-            <img class="img-responsive" src="<?php echo $ofbiz_url.$valueRawMaterials['artisianImage1'];?>" alt="Khadi" onerror="onImgError(this, 'PLP-Thumb');">
-          </div>
-          <div class="col-xs-12 col-sm-6 col-md-8 col-lg-8 right">
-            <h3><?php echo $valueRawMaterials['name']; ?></h3>
-            <p><?php echo $valueRawMaterials['description']; ?></p>
-            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-              <div class="row">
-                <div class="col-xs-12 col-sm-4 col-md-4 img-wrap">
-                  <img class="img-responsive" src="<?php echo $ofbiz_url.$valueRawMaterials['artisianImage2'];?>" alt="Khadi" onerror="onImgError(this, 'PLP-Thumb');">
-                </div>
-                <div class="col-xs-12 col-sm-4 col-md-4 img-wrap">
-                  <img class="img-responsive" src="<?php echo $ofbiz_url.$valueRawMaterials['artisianImage3'];?>" alt="Khadi" onerror="onImgError(this, 'PLP-Thumb');">
-                </div>
-                <div class="col-xs-12 col-sm-4 col-md-4 img-wrap">
-                  <img class="img-responsive" src="<?php echo $ofbiz_url.$valueRawMaterials['artisianImage4'];?>" alt="Khadi" onerror="onImgError(this, 'PLP-Thumb');">
-                </div>
-              </div>
-            </div>
-            <div class="btns-wrap">
-              <span><a class="ui-link" href="#" data-ajax="false">Find my Collections</a></span>
-            </div>
-          </div>
-        </div> <!-- content-wrap end -->
-
     </div>
   </div>
 </div>
-<?php endforeach; ?>
+<?php endforeach; 
+}?>
