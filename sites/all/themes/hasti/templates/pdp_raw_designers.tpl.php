8fd5f7954fc80276c517870746f9187f140476b8
Venusita Sontineni <venusita.s@sonata-software.com>
2018-01-08 13:50:22 +0530

    designers UI

--- sites/all/themes/hasti/templates/pdp_raw_designers.tpl.php
+++ sites/all/themes/hasti/templates/pdp_raw_designers.tpl.php
@@ -5,63 +5,40 @@ if(count($getRawMaterials['designers'])==0){?>
 <?php }else{
 foreach ($getRawMaterials['designers'] as $keyRawMaterials => $valueRawMaterials) :?>
 <div id="content" class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-  <div class="container-fluid" id="homepage">
-    <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-          <h3>DESIGNERS</h3>
-        </div>
-        <div class="content-wrap"> <!-- content-wrap -->
-          <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 left">
-            <img class="img-responsive" src="<?php echo $ofbiz_url.$valueRawMaterials['designerImage1'];?>" alt="Khadi" onerror="onImgError(this, 'PLP-Thumb');">
+  <div class="row">
+    <div class="container-fluid artisans" id="homepage">
+      <div class="row">
+          <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <div class="row">
+              <h3>DESIGNERS</h3>
+            </div>
           </div>
-          <div class="col-xs-12 col-sm-6 col-md-8 col-lg-8 right">
-            <h3><?php echo $valueRawMaterials['name']; ?></h3>
-            <p><?php echo $valueRawMaterials['description']; ?></p>
-            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-              <div class="row">
-                <div class="col-xs-12 col-sm-4 col-md-4 img-wrap">
-                  <img class="img-responsive" src="<?php echo $ofbiz_url.$valueRawMaterials['designerImage2'];?>" alt="Khadi" onerror="onImgError(this, 'PLP-Thumb');">
-                </div>
-                <div class="col-xs-12 col-sm-4 col-md-4 img-wrap">
-                  <img class="img-responsive" src="<?php echo $ofbiz_url.$valueRawMaterials['designerImage3'];?>" alt="Khadi" onerror="onImgError(this, 'PLP-Thumb');">
-                </div>
-                <div class="col-xs-12 col-sm-4 col-md-4 img-wrap">
-                  <img class="img-responsive" src="<?php echo $ofbiz_url.$valueRawMaterials['designerImage4'];?>" alt="Khadi" onerror="onImgError(this, 'PLP-Thumb');">
+          <div class="content-wrap"> <!-- content-wrap -->
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 left">
+              <img class="img-responsive" src="<?php echo $ofbiz_url.$valueRawMaterials['designerLogo'];?>" alt="Khadi" onerror="onImgError(this, 'PLP-Thumb');">
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-8 col-lg-8 right">
+              <h3><?php echo $valueRawMaterials['name']; ?></h3>
+              <p><?php echo $valueRawMaterials['description']; ?></p>
+              <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                <div class="row">
+                  <div class="col-xs-4 col-sm-4 col-md-4 pr0 img-wrap">
+                    <img class="img-responsive" src="<?php echo $ofbiz_url.$valueRawMaterials['designerImage1'];?>" alt="Khadi" onerror="onImgError(this, 'PLP-Thumb');">
+                  </div>
+                  <div class="col-xs-4 col-sm-4 col-md-4 pr0 img-wrap">
+                    <img class="img-responsive" src="<?php echo $ofbiz_url.$valueRawMaterials['designerImage2'];?>" alt="Khadi" onerror="onImgError(this, 'PLP-Thumb');">
+                  </div>
+                  <div class="col-xs-4 col-sm-4 col-md-4 pr0 img-wrap">
+                    <img class="img-responsive" src="<?php echo $ofbiz_url.$valueRawMaterials['designerImage3'];?>" alt="Khadi" onerror="onImgError(this, 'PLP-Thumb');">
+                  </div>
                 </div>
               </div>
-            </div>
-            <div class="btns-wrap">
-              <span><a class="ui-link" href="#" data-ajax="false">Find my Collections</a></span>
-            </div>
-          </div>
-        </div> <!-- content-wrap end -->
-
-        <div class="content-wrap"> <!-- content-wrap -->
-          <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4 left">
-            <img class="img-responsive" src="<?php echo $ofbiz_url.$valueRawMaterials['designerImage1'];?>" alt="Khadi" onerror="onImgError(this, 'PLP-Thumb');">
-          </div>
-          <div class="col-xs-12 col-sm-6 col-md-8 col-lg-8 right">
-            <h3><?php echo $valueRawMaterials['name']; ?></h3>
-            <p><?php echo $valueRawMaterials['description']; ?></p>
-            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-              <div class="row">
-                <div class="col-xs-12 col-sm-4 col-md-4 img-wrap">
-                  <img class="img-responsive" src="<?php echo $ofbiz_url.$valueRawMaterials['designerImage2'];?>" alt="Khadi" onerror="onImgError(this, 'PLP-Thumb');">
-                </div>
-                <div class="col-xs-12 col-sm-4 col-md-4 img-wrap">
-                  <img class="img-responsive" src="<?php echo $ofbiz_url.$valueRawMaterials['designerImage3'];?>" alt="Khadi" onerror="onImgError(this, 'PLP-Thumb');">
-                </div>
-                <div class="col-xs-12 col-sm-4 col-md-4 img-wrap">
-                  <img class="img-responsive" src="<?php echo $ofbiz_url.$valueRawMaterials['designerImage4'];?>" alt="Khadi" onerror="onImgError(this, 'PLP-Thumb');">
-                </div>
+              <div class="btns-wrap">
+                <span><a class="ui-link" href="#" data-ajax="false">Find my Collections</a></span>
               </div>
             </div>
-            <div class="btns-wrap">
-              <span><a class="ui-link" href="#" data-ajax="false">Find my Collections</a></span>
-            </div>
-          </div>
-        </div> <!-- content-wrap end -->
-
+          </div> <!-- content-wrap end -->
+      </div>
     </div>
   </div>
 </div>	
